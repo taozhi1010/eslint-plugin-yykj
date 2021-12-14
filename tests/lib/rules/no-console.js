2b6111761df995a3188ffbff7a9468feb1cd0a98
taozhi <805446529@qq.com>
2021-12-14 09:53:22 +0800

    feat add:rule no-console

--- tests/lib/rules/no-console.js
+++ tests/lib/rules/no-console.js
@@ -16,15 +16,20 @@ const rule = require("../../../lib/rules/no-console"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 7, // 默认支持语法为es5 
+  },
+});
 ruleTester.run("no-console", rule, {
   valid: [
     // give me some code that won't trigger a warning
+
   ],
 
   invalid: [
     {
-      code: "代办",
+      code: "console.log(0000)",
       errors: [{ message: "Fill me in.", type: "Me too" }],
     },
   ],
